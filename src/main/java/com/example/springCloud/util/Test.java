32f82900d700190c7cb76dc2fd6d9a98873bec64
hejun <1181367274@qq.com>
2023-02-27 18:47:10 +0800

    修改loadbalance openfeign 版本

--- src/main/java/com/example/springCloud/util/Test.java
+++ src/main/java/com/example/springCloud/util/Test.java
@@ -1,8 +1,14 @@
 package com.example.springCloud.util;
 
+import com.alibaba.nacos.client.utils.ParamUtil;
+
 public class Test {
     public static void main(String[] args) {
-        String projectPath = System.getProperty("user.dir");
-        System.out.println(projectPath);
+//        String projectPath = System.getProperty("user.dir");
+//        System.out.println(projectPath);
+
+
+        int longingTaskCount = (int)Math.ceil((double)2000 / ParamUtil.getPerTaskConfigSize());
+        System.out.println(longingTaskCount);
     }
 }
