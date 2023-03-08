9f3c9749ec9d78f8833697d26b2401445cc6b6e5
hejun <1181367274@qq.com>
2023-03-08 14:59:37 +0800

    配置使用kafka 进行日志输入 将kafka信息进行配置文件配置

---
+++ src/main/java/com/example/springCloud/config/IPConverterConfig.java
@@ -0,0 +1,21 @@
+package com.example.springCloud.config;
+
+
+import ch.qos.logback.classic.pattern.ClassicConverter;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class IPConverterConfig extends ClassicConverter {
+
+    @Override
+    public String convert(ILoggingEvent iLoggingEvent) {
+        try {
+            return InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
